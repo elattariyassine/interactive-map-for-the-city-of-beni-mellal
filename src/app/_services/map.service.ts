0b92b254b316200daed1612b91d54837ee3b5b7f
El attari Yassine <yassineattari64@gmail.com>
2020-01-24 16:10:59 +0100

    [Coding] update amp component & service

--- src/app/_services/map.service.ts
+++ src/app/_services/map.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as L from 'leaflet';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
 
-  constructor() { }
+  places: string = 'http://localhost:3000/markers';
+
+  constructor(private http: HttpClient) {
+  }
+
+  makePlacesMarkers(map: L.map, type): void {
+    this.http.get(this.places).subscribe((res: any) => {
+      for (const c of res) {
+        if (c.type == type){
+          const lat = c.coordinates[0];
+          const lon = c.coordinates[1];
+          const marker = L.marker([lon, lat]).addTo(map);
+          marker.bindPopup(`<center>
+                              <p>
+                                <strong>${c.name}</strong>
+                              </p>
+                            </center>
+                            <img style="max-width: -webkit-fill-available;" src="${c.image}"/><br />
+                            <p>${c.description}</p>
+                              `).openPopup();
+        }
+      }
+    });
+  }
 }
--- src/app/examples/landing/map/map.component.ts
+++ src/app/examples/landing/map/map.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { MapService } from 'app/_services/map.service';
+import * as L from 'leaflet';
+
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
-export class MapComponent implements OnInit {
+export class MapComponent implements AfterViewInit {
 
-  constructor() { }
+  private map;
 
-  ngOnInit() {
+  constructor(private mapService: MapService) { }
+
+  ngAfterViewInit(): void {
+    this.initMap();
+    this.mapService.makePlacesMarkers(this.map, "A");
   }
+
+  private initMap(): void {
+    this.map = L.map('map').setView([32.339444, -6.360833], 16);     
+      //   https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}
+      //   https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png
+      //   https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png
+      const tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?access_token={accessToken}', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      accessToken: 'pk.eyJ1IjoiZXJ3aW5yYXJlIiwiYSI6ImNrNW56eXkzdzExazIzanFwa3ptbmd2ZjQifQ.W-hmr4OR8LyBvM9j8AgSHw',
+      id: 'mapbox/streets-v11'
+    });
+
+  tiles.addTo(this.map);
+  }
+ 
 
 }
